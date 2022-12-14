a59143c444d68284a26e81c38d49ee565ab86857
Daniel Gonzalez De la Oliva <dgonzalezdelaoliva@mecalux.com>
2022-12-14 17:29:02 +0100

    reajuste de carpetas

---
+++ NetCore5/AccountOwnerServer/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountOwnerServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //habilitamos Cors para poder acceder a los metodos desde un dominio diferentes. (por ejemplo Angular)
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        //habilitamos la configuración personaliza de IIS
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+            });
+        }
+
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
---
+++ NetCore5/AccountOwnerServer/AccountOwnerServer/Startup.cs
@@ -0,0 +1,73 @@
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog;
+using System;
+using System.Configuration;
+using System.IO;
+
+namespace AccountOwnerServer
+{
+    public class Startup
+    {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //agremos servicio para deshabilitar la resticciones Cors
+            services.ConfigureCors();
+
+            //agregamos servicio para configuración personalizado de IIS
+            services.ConfigureIISIntegration(); 
+
+            //agregamos el registro de errores (log) solo una instancia por ejecución
+            services.ConfigureLoggerService();
+
+            //agregamos servicio para visualizar controllers
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            app.UseRouting();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+           
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
